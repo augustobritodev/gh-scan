dab7ce67801af52eb195a078348619d62070906d
Augusto <augustobrito@outlook.com>
2020-03-10 08:45:23 -0300

    finished base app

--- src/App.js
+++ src/App.js
@@ -12,39 +12,96 @@ function App() {
   const [userInput, setUserInput] = useState("");
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetch("https://api.github.com/users/example")
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+      });
+  }, []);
+
+  const setData = ({
+    name,
+    login,
+    followers,
+    following,
+    public_repos,
+    avatar_url
+  }) => {
+    setName(name);
+    setUsername(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+  };
+
+  const handleSearch = e => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.message) {
+          setError(data.message);
+        } else {
+          setData(data);
+          setError(null);
+        }
+      });
+  };
+
   return (
     <div>
       <div className="navbar">Github Search</div>
       <div className="search">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
-            <Form.Input placeHolder="Github user" name="github user" />
+            <Form.Input
+              placeHolder="Github user"
+              name="github user"
+              onChange={handleSearch}
+            />
             <Form.Button content="Search" />
           </Form.Group>
         </Form>
       </div>
-      <div className="card">
-        <Card>
-          <Image
-            src="https://react.semantic-ui.com/images/avatar/large/matthew"
-            wrapped
-            ui={false}
-          />
-          <Card.Content>
-            <Card.Header>Augusto Brito</Card.Header>
-            <Card.Meta>
-              <span className="date">Joined in 2015</span>
-            </Card.Meta>
-            <Card.Description>Software Developer</Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              22 Friends
-            </a>
-          </Card.Content>
-        </Card>
-      </div>
+      {error ? (
+        <h1>{error}</h1>
+      ) : (
+        <div className="card">
+          <Card>
+            <Image src={avatar} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{name}</Card.Header>
+              <Card.Meta>
+                <span className="date">{userName}</span>
+              </Card.Meta>
+              <Card.Description></Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                {followers} Followers
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="hdd outline icon" />
+                {repos} Repositories
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="users" />
+                {following} Following
+              </a>
+            </Card.Content>
+          </Card>
+        </div>
+      )}
     </div>
   );
 }
